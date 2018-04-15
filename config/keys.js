96795fd10cc84b15bdbd5bd6f3b8e91f6d05bcae
Nick Snyder <n.a.snyder@comcast.net>
2018-04-15 17:30:21 -0400

    handled auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://Nick:nick@ds239965.mlab.com:39965/storybooks-dev',
+  googleClientID: '841521340928-6aekv6bq00sj9tbgogsjlojfsk3su8uf.apps.googleusercontent.com',
+  googleClientSecret: 'JRjn_HGQZsymgvQ-KwegxpZp'
+}
