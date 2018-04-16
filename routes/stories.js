f1879802f3a676706312606c9865a0bca324e4bd
Nick Snyder <n.a.snyder@comcast.net>
2018-04-15 21:38:22 -0400

    Initial github init

--- routes/stories.js
+++ routes/stories.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Story = mongoose.model('stories');
+const User = mongoose.model('users');
 const { ensureAuthenticated, ensureGuest } = require('../helpers/auth');
 
 
@@ -11,6 +14,32 @@ router.get('/', (req, res) => {
 // Add Story form
 router.get('/add', ensureAuthenticated, (req, res) => {
   res.render('stories/add');
+});
+
+// Process Add Story
+router.post('/', (req, res) => {
+  let allowComments;
+
+  if(req.body.allowComments){
+    allowcomments = true;
+  } else{
+    allowComments = false;
+  }
+
+  const newStory = {
+    title: req.body.title,
+    body: req.body.body,
+    status: req.body.status,
+    allowComments: allowComments,
+    user: req.user.id
+  }
+
+  // Create Story
+  new Story(newStory)
+    .save()
+    .then(story => {
+      res.redirect(`/stories/show/${story.id}`);
+    });
 });
 
 module.exports = router;
