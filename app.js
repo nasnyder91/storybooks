79a95ee730f0174d5c9697b2c8259bb0dc840092
Nick Snyder <n.a.snyder@comcast.net>
2018-04-15 20:19:47 -0400

    added add page

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
@@ -14,6 +15,8 @@ require('./config/passport')(passport);
 // Load Routes
 const auth = require('./routes/auth');
 const index = require('./routes/index');
+const stories = require('./routes/stories');
+
 
 
 // Load keys
@@ -51,11 +54,13 @@ app.use((req, res, next) => {
   next();
 });
 
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Use Routes
 app.use('/', index);
 app.use('/auth', auth);
-
-
+app.use('/stories', stories);
 
 const port = process.env.PORT || 5000;
 
---
+++ routes/stories.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const { ensureAuthenticated, ensureGuest } = require('../helpers/auth');
+
+
+// Stories index
+router.get('/', (req, res) => {
+  res.render('stories/index');
+});
+
+// Add Story form
+router.get('/add', ensureAuthenticated, (req, res) => {
+  res.render('stories/add');
+});
+
+module.exports = router;
